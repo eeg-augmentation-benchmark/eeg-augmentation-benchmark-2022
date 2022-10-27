1df778f7359581b1297d18dbdabf2b2fd4fcfa2f
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2022-10-27 17:46:47 +0200

    more fixes

--- param_search/plot_all.sh
+++ param_search/plot_all.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat frequency
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat frequency --png
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat time --col
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat time --col --png
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat sensors --col
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat sensors --col --png
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat rotations
-python plots/plot_param_search.py outputs/physionet/learning_curves/ps_all_transforms.pkl --tfs-cat rotations --png
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat frequency
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat frequency --png
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat time --col
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat time --col --png
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat sensors --col
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat sensors --col --png
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat rotations
+python plots/plot_param_search.py outputs/physionet/param_search/ps_all_transforms.pkl --tfs-cat rotations --png
 
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat frequency
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat frequency --png
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat time --col
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat time --col --png
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat sensors --col
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat sensors --col --png
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat rotations
-python plots/plot_param_search.py outputs/BCI/learning_curves/ps_all_transforms.pkl --tfs-cat rotations --png
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat frequency
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat frequency --png
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat time --col
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat time --col --png
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat sensors --col
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat sensors --col --png
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat rotations
+python plots/plot_param_search.py outputs/BCI/param_search/ps_all_transforms.pkl --tfs-cat rotations --png
